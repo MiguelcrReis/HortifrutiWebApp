f4b339beacbbe348b91d3711bbb9255ac5b90cd2
Miguel Reis <miguel.reis@megalaudo.com.br>
2023-01-06 19:39:21 -0300

    Created ClientStatus

---
+++ HortifrutiWebApp/Models/Enums/ClientStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HortifrutiWebApp.Models.Enums
+{
+    public enum ClientStatus
+    {
+        Registered,
+        Approved,
+        Blocked,
+        Special
+    }
+}
