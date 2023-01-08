51a56b108c43efcebe14eaf63fafbde1088a8310
MiguelCrReis <miguelcr.reis@gmail.com>
2023-01-08 17:12:39 -0300

    Created Enum OrderStatus

---
+++ HortifrutiWebApp/Models/Enums/OrderStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HortifrutiWebApp.Models.Enums
+{
+    public enum OrderStatus
+    {
+        Canceled,
+        Realized,
+        Verified,
+        Answered,
+        Delivered
+    }
+}
