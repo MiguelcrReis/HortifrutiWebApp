61cd90b101688e0caacad257a0ecfcfa9c5cee35
MiguelCrReis <miguelcr.reis@gmail.com>
2023-01-08 17:18:33 -0300

    Created table for new entities

--- HortifrutiWebApp/Data/WebAppDbContext.cs
+++ HortifrutiWebApp/Data/WebAppDbContext.cs
@@ -1,4 +1,4 @@
-﻿using HortifrutiWebApp.Models;
+﻿using HortifrutiWebApp.Models.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace HortifrutiWebApp.Data
@@ -10,7 +10,20 @@ namespace HortifrutiWebApp.Data
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<OrderItem>().HasKey(x => new { x.OrderId, x.ProductId });
+
+            modelBuilder.Entity<Favorite>().HasKey(x => new { x.ClientId, x.ProductId });
+
+            modelBuilder.Entity<Visit>().HasKey(x => new { x.ClientId, x.ProductId });
+        }
+
         public DbSet<Product> Product { get; set; }
-        public DbSet<Client> Clients { get; set; }
+        public DbSet<Client> Clients { get; set; }  
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Favorite> Favorites { get; set; }
+        public DbSet<Visit> Visits { get; set; }
     }
 }
