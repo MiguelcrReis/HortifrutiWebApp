823f9b2163ec02117c64a33b2fb817084bb15348
Miguel Reis <miguel.reis@megalaudo.com.br>
2023-01-11 15:21:29 -0300

    Script for popular tb products

---
+++ HortifrutiWebApp/load_tb_products.sql
@@ -0,0 +1,29 @@
+INSERT INTO `hortifrutireis`.`products`
+(`Name`,`Descrition`,`Price`,`Stock`)
+VALUES
+('Maçã Verde', 'Originária da Austrália, a fruta é conhecida por ser mais azeda do que as outras versões.', '12.50', '120'),
+('Maçã Gala', 'Crocante, doce e com um ligeiro fundo ácido.', '13.20', '100'),
+('Laranja', 'A laranja é um fruto de várias espécies cítricas da família Rutaceae.', '14.20', '150'),
+('Banana Prata', 'Tem bastante açucares naturais, como a sacarose, frutose, e glicose, além de sais minerais.', '8.30', '130'),
+('Mamão Papaia', 'Carica papaya é uma espécie de fruteira tropical que produz os frutos conhecidos pelos nomes comerciais de papaia ou ababaia.', '9.50', '100'),
+('Uva', 'A uva é o fruto da videira, uma planta da família das Vitaceae.', '7.99', '150'),
+('Uva Verde', 'As uvas verdes são frutas ricas em potássio, vitamina A, ácidos orgânicos e compostos fenólicos.', '8.99', '90'),
+('Melancia', 'A melancia é uma espécie de planta com flor da família Cucurbitaceae e o nome de sua fruta comestível.', '4.11', '80'),
+('Limão Siciliano', ' limão é uma espécie de pequenas árvores perenes da família de plantas com flores Rutaceae.', '8.50', '10'),
+('Limão', 'Limão é o fruto do limoeiro, uma pequena árvore de folha perene originária da região sudeste da Ásia.', '6.20', '30'),
+('Pessego', 'O pêssego é rico em fibras, bom para o funcionamento do intestino e contém minerais, carboidratos, proteínas e vitaminas.', '9.50', '50'),
+('Abacaxi', 'O ananás ou abacaxi é uma infrutescência tropical produzida pela planta de mesmo nome.', '4.50', '70'),
+('Uva Thompson Seedless', 'A uva Thompson Seedless é considerada a mais importante uva naturalmente sem sementes no mundo.', '3.10', '40'),
+('Avocado', 'O abacate é o fruto comestível do abacateiro.', '4.40', '50'),
+('Pimenta dedo-de-moça', 'Capsicum baccatum é o nome científico de uma pimenta conhecida como pimenta dedo-de-moça.', '4.20', '50'),
+('Pepino Japonês', 'O pepino é um diurético natural e de grande ajuda na dissolução de cálculos renais.', '2.50', '40'),
+('Cereja', 'A cereja é o fruto de muitas plantas do gênero Prunus, e é uma drupa carnuda.', '2.30', '50'),
+('Tomate', 'O tomate é o fruto do tomateiro.', '12.11', '40'),
+('Pimentão Vermelho', 'Rico em vitamina A.', '21.10', '20'),
+('Amora', 'Ela é o fruto da amoreira, uma árvore comumente encontrada no Brasil', '32.20', '100'),
+('Mirtilo', 'O mirtilo é conhecido como blueberry, em inglês, e arándano, em espanhol, incluído-se no grupo das pequenas frutas.', '2.35', '110'),
+('Pimentão Verde', 'Por não ter amadurecido ainda, o seu sabor é o mais azedo dos três.', '13.56', '110'),
+('Pimentão Amarelo', 'Aqui quem rouba a atenção é a vitamina C, já que ela aparece em grandes quantidades no pimentão amarelo.', '11.12', '100'),
+('Manga Coração de Boi', 'Coração de Boi é uma cultivar de origem brasileira de maturação na meia estação.', '8.40', '40'),
+('Kiwi', 'Kiwi ou groselha chinesa é a baga comestível de várias espécies de videiras lenhosas no gênero Actinidia.', '12.00', '40');
+
