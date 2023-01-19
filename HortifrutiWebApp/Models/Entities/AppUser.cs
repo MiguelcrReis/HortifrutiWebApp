65bef95b0ddd2a09313255c99caa144647188bff
Miguel Reis <miguel.reis@megalaudo.com.br>
2023-01-19 15:50:06 -0300

    Created AppUser class

---
+++ HortifrutiWebApp/Models/Entities/AppUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace HortifrutiWebApp.Models.Entities
+{
+    public class AppUser : IdentityUser
+    {
+        public string Nome { get; set; }
+    }
+}
