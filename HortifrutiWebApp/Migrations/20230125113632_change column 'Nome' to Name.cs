98e05578779af4edc3cc68130abfce14f079495f
Miguel Reis <miguel.reis@megalaudo.com.br>
2023-01-25 08:56:08 -0300

    Created new Migration Changed the column 'Nome' to Name in the User Table

---
+++ HortifrutiWebApp/Migrations/20230125113632_change column 'Nome' to Name.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HortifrutiWebApp.Migrations
+{
+    public partial class changecolumnNometoName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Nome",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Nome",
+                table: "AspNetUsers",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+        }
+    }
+}
