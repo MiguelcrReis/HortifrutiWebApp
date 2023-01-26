ba53cb7bbd3a18a87098b8b17195113c8f332eb5
Miguel Reis <miguel.reis@megalaudo.com.br>
2023-01-26 14:55:41 -0300

    Created email configuration entity

---
+++ HortifrutiWebApp/Entities/EmailConfiguration.cs
@@ -0,0 +1,13 @@
+﻿
+namespace HortifrutiWebApp.Entities
+{
+    public class EmailConfiguration
+    {
+        public string SenderName { get; set; }
+        public string SenderEmail { get; set; }
+        public string Password { get; set; }
+        public string ServerAddressEmail { get; set; }
+        public string ServerPortEmail { get; set; }
+        public bool UserSSL { get; set; }
+    }
+}
