0374d7abea04378ca9cb0d68627d6b0b6baa6913
MiguelCrReis <miguelcr.reis@gmail.com>
2023-02-05 21:44:56 -0300

    Finishing shopping cart modeling

--- HortifrutiWebApp/Pages/ShoppingCart.cshtml.cs
+++ HortifrutiWebApp/Pages/ShoppingCart.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using HortifrutiWebApp.Data;
 using HortifrutiWebApp.Models.Entities;
+using HortifrutiWebApp.Models.Enums;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -16,7 +17,7 @@ namespace HortifrutiWebApp.Pages
         private WebAppDbContext _context;
         private SignInManager<AppUser> _signInManager;
         private UserManager<AppUser> _userManager;
-        private const string COOKIE_NAME = ".AspNetCore.CartId";
+        public string COOKIE_NAME { get { return ".AspNetCore.CartId"; } }
 
         public Order Order { get; set; }
         public decimal Amount { get; set; }
@@ -63,9 +64,73 @@ namespace HortifrutiWebApp.Pages
             return cartId;
         }
 
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAddToCartAsync(int? id, int qtd = 1)
         {
+            if (id == null) return NotFound();
 
+            var product = await _context.Products.FindAsync(id);
+
+            if (product != null)
+            {
+                string cartId = null;
+
+                if (Request.Cookies.ContainsKey(COOKIE_NAME))
+                {
+                    cartId = Request.Cookies[COOKIE_NAME];
+                    Order = await _context.Orders.Include("OrderItems").Include("OrderItems.Product").FirstOrDefaultAsync(p => p.CartId == cartId);
+                }
+                else
+                {
+                    cartId = SetCartCookie();
+                }
+
+                if (Order == null)
+                {
+                    Order = new Order
+                    {
+                        CartId = cartId,
+                        DateTimeOrder = DateTime.UtcNow,
+                        OrderStatus = OrderStatus.ShoppingCart,
+                        OrderItems = new List<OrderItem>()
+                    };
+
+                    AppUser appUser = _signInManager.IsSignedIn(User) ? await _userManager.GetUserAsync(User) : null;
+
+                    if (appUser != null)
+                    {
+                        Client client = await _context.Clients.FirstOrDefaultAsync<Client>(c => c.Email.ToLower().Equals(appUser.Email.ToLower()));
+
+                        if (client != null) Order.ClientId = client.ClientId;
+                    }
+
+                    _context.Orders.Add(Order);
+                }
+
+                var orderItem = Order.OrderItems.FirstOrDefault(oi => oi.ProductId == id);
+
+                if (orderItem == null)
+                {
+                    Order.OrderItems.Add(new OrderItem
+                    {
+                        OrderId = id.Value,
+                        Quantity = qtd,
+                        UnitaryValue = product.Price.Value
+                    });
+                }
+                else
+                {
+                    orderItem.Quantity += qtd;
+                }
+
+                if (_context.SaveChanges() <= 0)
+                {
+                    ModelState.AddModelError("", "Ocorreu um erro ao tentar adicionar o item ao carrinho.");
+                }
+            }
+
+            Amount = Order.OrderItems.Sum(x => Convert.ToDecimal(x.Quantity) * x.UnitaryValue);
+
+            return Page();
         }
     }
 }
