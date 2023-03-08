c5746e4704afe1a66954c77fe5ead308115b4067
MiguelCrReis <miguelcr.reis@gmail.com>
2023-03-07 22:50:44 -0300

    Created extension to adapt display of Enum OrderStatus

---
+++ HortifrutiWebApp/Models/Enums/EnumExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace HortifrutiWebApp.Models.Enums
+{
+    public static class EnumExtensions
+    {
+        public static string GetDisplayAttributeFrom(Enum enumValue, Type enumType)
+        {
+            MemberInfo info = enumType.GetMember(enumValue.ToString()).First();
+
+            string displayName;
+            if (info != null && info.CustomAttributes.Any())
+            {
+                DisplayAttribute nameAttr = info.GetCustomAttribute<DisplayAttribute>();
+                displayName = nameAttr != null ? nameAttr.Name : enumValue.ToString();
+            }
+            else
+            {
+                displayName = enumValue.ToString();
+            }
+            return displayName;
+        }
+    }
+}
--- HortifrutiWebApp/Models/Enums/OrderStatus.cs
+++ HortifrutiWebApp/Models/Enums/OrderStatus.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,17 @@ namespace HortifrutiWebApp.Models.Enums
 {
     public enum OrderStatus
     {
+        [Display(Name = "Carrinho de Compras")]
         ShoppingCart,
+        [Display(Name = "Cancelado")]
         Canceled,
+        [Display(Name = "Realizado")]
         Realized,
+        [Display(Name = "Verificado")]
         Verified,
+        [Display(Name = "Atendido")]
         Answered,
+        [Display(Name = "Entregue")]
         Delivered
     }
 }
